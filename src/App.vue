cf225241e36e625c779832a806a575dd9f19bdfd
小乌龟 <visoeclipse@gmail.com>
2023-07-20 19:04:40 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <topology />
+  </div>
+</template>
+
+<script>
+import topology from "topology-vue";
+import "topology-vue/topology-vue.css";
+
+export default {
+  components: {
+    topology
+  },
+};
+</script>
+<style lang="scss">
+.home {
+  height: calc(100vh - 80px);
+}
+</style>
